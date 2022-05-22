aa81e67bfcb0c6c3e74aa2331bf3c4f56f87ee0c
Massoud1319 <ahmedmassoud1319@gamil.com>
2022-05-22 15:46:17 +0200

    pushing files

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+        item.classList.toggle("change");
+    })
+})
+
+
+const icons = document.querySelectorAll('.section-1-icons i')
+let i = 1
+setInterval(() => {
+        i++
+        const icon = document.querySelector('.section-1-icons .change')
+        icon.classList.remove('change')
+        if (i > icons.length) {
+            icons[0].classList.add('change')
+            i = 1
+        } else {
+            icon.nextElementSibling.classList.add('change')
+        }
+    }, 5000)
+    // const icons = document.querySelector(".varietyOfFoodContainer .verityFood .icons i");
+    // let i = 1
+    // setInterval(() => {
+    //     i++;
+    //     const icon = document.querySelector(".varietyOfFoodContainer .verityFood .icons .change");
+    //     icon.classList.remove("change")
+
+//     if (i > icons.length) {
+//         icon[0].classList.remove("change");
+//         i = 1
+
+//     } else {
+//         icon.nextElementSibling.classList.add("change")
+//     }
+// }, 1000)
